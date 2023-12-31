6d4917798eb00764d1dfda8f2ea9bda6a85e6781
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-31 08:16:22 -0600

    Update feature_imp.py

--- ML_Pipeline/feature_imp.py
+++ ML_Pipeline/feature_imp.py
@@ -6,6 +6,6 @@ def plot_feature_importances(model):
   feature_importances = pd.Series(model.feature_importances_, index=model.feature_names_in_)
   feature_importances = feature_importances.sort_values(axis=0, ascending=False)
   fig, ax = plt.subplots()
-  feature_importances.plot.bar()
+  feature_importances.plot.bar() 
   ax.set_title("Feature importances")
   fig.tight_layout()
